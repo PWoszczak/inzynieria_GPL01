dd008c63939169248da023f28dd5b0cc94d48ba1
Piotr Woszczak <w64241@student.wsiz.edu.pl>
2023-03-20 12:59:25 +0100

    't'

--- lab1/UnitTest1.cs
+++ lab1/UnitTest1.cs
@@ -8,12 +8,22 @@ namespace lab1
 {
     public class UnitTest1
     {
+
         [Fact]
         public void Test1()
         {
-            int i = 0;
-            i++;
-            Console.WriteLine();
+            Faker<Address> addressBogus = new Faker<Address>();
+            addressBogus.RuleFor(x => x.Description, y => y.Address.FullAddress());
+            addressBogus.RuleFor(x => x.Number, y => y.Address.CountryCode());
+
+            var personBogus = new Faker<Person>();
+            personBogus.RuleFor(x => x.Name, y => y.Person.FirstName);
+            personBogus.RuleFor(x => x.Surname, y => y.Person.LastName);
+            personBogus.RuleFor(x => x.Birthday, y => y.Person.DateOfBirth);
+            personBogus.RuleFor(x => x.Address, y => addressBogus.Generate());
+            var hundredPpl = personBogus.Generate(100);
+
+            Console.WriteLine("aa");
         }
     }
 }
