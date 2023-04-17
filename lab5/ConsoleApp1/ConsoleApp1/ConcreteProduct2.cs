4f723f3a2e0bfbea2350023a14638e4a1de29617
Piotr Woszczak <w64241@student.wsiz.edu.pl>
2023-04-17 14:18:29 +0200

    com5

---
+++ lab5/ConsoleApp1/ConsoleApp1/ConcreteProduct2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct2}";
+        }
+    }
+}
